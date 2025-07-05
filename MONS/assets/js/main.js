156325b9515035ff078a27b9a6c2a67c23d7c349
iSmile <hromcikfilip@gmail.com>
2025-07-05 06:26:40 +0200

    Add files via upload

---
+++ MONS/assets/js/main.js
@@ -0,0 +1,12 @@
+const frame = document.getElementById('game');
+const buttons = document.querySelectorAll('.controls');
+    
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        frame.contentWindow.postMessage({
+            action: button.classList.contains('up') ? 'up' : 
+                    button.classList.contains('down') ? 'down' : 
+                    'select'
+        }, '*');
+    });
+});
