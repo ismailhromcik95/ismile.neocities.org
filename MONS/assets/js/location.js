e4a74d71a4ce078c3ba30defb59334caa11a5086
iSmile <hromcikfilip@gmail.com>
2025-07-06 03:43:54 +0200

    Add files via upload

---
+++ MONS/assets/js/location.js
@@ -0,0 +1,16 @@
+window.addEventListener('message', (event) => {
+    const radios = document.getElementsByName('location-menu');
+    const currentIndex = Array.from(radios).findIndex(radio => radio.checked);
+    
+    switch(event.data.action) {
+        case 'up':
+        case 'down':
+            const nextIndex = (currentIndex + 1) % radios.length;
+            radios[nextIndex].checked = true;
+            break;
+            
+        case 'select':
+            console.log('Select pressed - current selection:', radios[currentIndex].value);
+            break;
+    }
+});
